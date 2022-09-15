64d27c5ecf068fbc4faacb58f5aba5c235611271
krao14 <108151318+krao14@users.noreply.github.com>
2022-09-14 17:06:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def predict():
     return render_template('index.html', prediction_text=prediction_text)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port='8501')
+    app.run(debug=True, host='0.0.0.0', port='80')
